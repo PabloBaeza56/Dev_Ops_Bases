d70f318d5f41749e5f0d417e08c19c3d46d1ca46
PabloBaeza56 <75227439+PabloBaeza56@users.noreply.github.com>
2023-06-04 19:42:06 -0600

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	e := echo.New()
-	.GET("/ping", func(c echo.Context) error {
+	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "POoofffNG!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
