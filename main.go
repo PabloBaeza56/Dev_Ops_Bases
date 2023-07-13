12528aeee5f7f9f4977120873b785f8327153345
PabloBaeza56 <75227439+PabloBaeza56@users.noreply.github.com>
2023-07-13 16:11:38 -0600

    Update main.go

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/ping", func(c echo.Context) error {
-		return c.String(http.StatusOK, "88hhffPOOOOOggggggOOOFFFFFFFFFFFFFNG!")
+		return c.String(http.StatusOK, "6988hhffPOOOOOggggggOOOFFFFFFFFFFFFFNG!")
 	})
 	e.GET("/pong", func(c echo.Context) error {
-		return c.String(http.StatusOK, "88hhffPIIIIIggggggIFFFFFFFFFFFFNG!")
+		return c.String(http.StatusOK, "6988hhffPIIIIIggggggIFFFFFFFFFFFFNG!")
 	})
 	e.GET("/bum", func(c echo.Context) error {
-		return c.String(http.StatusOK, "88hhffBOOOOggggggOFFFFFFFFFFM!")
+		return c.String(http.StatusOK, "6988hhffBOOOOggggggOFFFFFFFFFFM!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
